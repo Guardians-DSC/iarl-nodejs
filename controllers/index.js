52c587320a8f9e4b0ead6c9051cbc021cc1ab451
daniboyrc <danielrcifpb@gmail.com>
2017-12-13 05:30:08 -0300

    listing servers

--- controllers/index.js
+++ controllers/index.js
@@ -5,8 +5,12 @@ module.exports = function(app) {
             if (!req.session.user) {
                 res.status(500).json({message: "User isn't logged in!"})
             } else {
+                // load servers
+                var servers = require('../public/js/servers.json');
+                
                 res.status(200).json({
                     title: "Workspace",
+                    servers: servers,
                 });
             }
         }
