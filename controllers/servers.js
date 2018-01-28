18f35504af3925f9c5d22c1647a7d527b2aea810
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2018-01-28 08:14:31 -0300

    moving servers.json to root

--- controllers/servers.js
+++ controllers/servers.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
     var indexControl = {
         get: function(req, res) {
             // load servers
-            var servers = require('../public/js/servers.json');
+            var servers = require('../servers.json');
             
             res.status(200).json({
                 servers: servers,
