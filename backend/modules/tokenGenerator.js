44d4640d707ad7e61349ac25139eff03bb6207ad
Daniel Rodrigues Coura <danielrc@regressao.analytics.ufcg.edu.br>
2018-05-15 13:05:52 -0300

    Expiration time in token

--- backend/modules/tokenGenerator.js
+++ backend/modules/tokenGenerator.js
@@ -3,9 +3,8 @@ const config = require('config')
 
 
 function sendToken(req) {
-  const token = jwt.sign({ username: req.body.username }, config.get('jwtPrivateKey'))
+  const token = jwt.sign({ username: req.body.username }, config.get('jwtPrivateKey'), { expiresIn: 60 * 20 })
   return token
 }
 
 module.exports = sendToken
-  
