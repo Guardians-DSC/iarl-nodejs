2fab04f0eeab9fd70514a079b4d21850a3869390
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2018-06-12 21:33:36 -0300

    Handle unexpected exceptions

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ consign()
 // handle errors
 require('./startup/error')(app)
 
+// handle unexpected errors
+process.on('uncaughtException', function(err) {
+	console.log(err)
+})
+
 // server listen on defined port
 var port = process.env.PORT || 3000;
 app.listen(port, () => { console.log(`server listen on port ${port}...`) })
