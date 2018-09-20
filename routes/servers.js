04ed0c8c12fe58e8a684cf6f3070253cd60c206c
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2018-09-19 21:24:32 -0300

    Add servers list documentation

--- routes/servers.js
+++ routes/servers.js
@@ -1,4 +1,31 @@
 module.exports = function (app) {
   const servers = app.controllers.servers;
+
+  /**
+   * @api {get} /api/servers List servers
+   * @apiGroup Servers
+   *
+   * @apiDescription
+   * Lists all servers that the client can connect to have access to user directories.
+   *
+   * @apiSuccess {String} servers An array with server names, addresses, and ports.
+   *
+   * @apiSuccessExample {json} Success
+   * HTTP/1.1 200 OK
+   * {
+   *    "servers": [
+   *        {
+   *            "name": "lcc1",
+   *            "host": "chopper.lcc.ufcg.edu.br",
+   *            "port": "23456"
+   *        },
+   *        {
+   *            "name": "lcc2",
+   *            "host": "r2d2.lcc.ufcg.edu.br",
+   *            "port": "23456"
+   *        }
+   *    ]
+   * }
+   */
   app.get('/api/servers', servers.get);
 };
