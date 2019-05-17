63b287b2f41a681ff30d297c19733f89e6be43fa
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-05-17 13:02:37 -0300

    Create lcc body getter to extract lcc reference from requests

---
+++ utils/lcc-body-getter.js
@@ -0,0 +1,15 @@
+module.exports = (req) => {
+    var lcc;
+    switch (req.body.lcc) {
+        case '1':
+        lcc = '/lcc1' // TODO update this value to be recover by environment variable
+        break;
+        case '2':
+        lcc = '/lcc2' // TODO update this value to be recover by environment variable
+        break;
+        default:
+        lcc = '/lcc3' // TODO update this value to be recover by environment variable
+        break;
+    }
+    return lcc;
+};
