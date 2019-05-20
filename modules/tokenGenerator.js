f563e896ee0789d38a76a153ecd797e5949bfbfb
Daniel Rodrigues Coura <daniel.coura@ccc.ufcg.edu.br>
2019-05-20 13:32:44 -0300

    Update tokenGenerator.js

--- modules/tokenGenerator.js
+++ modules/tokenGenerator.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 function sendToken (req) {
-  const token = jwt.sign({ username: req.body.username }, config.get('jwtPrivateKey'));
+  const token = jwt.sign({ username: req.body.username }, config.get('jwtPrivateKey'), { expiresIn: '7 days' });
   return token;
 }
 
