3b03e34d2753b8e7ba5eda4f7d4beb02d697f0f0
amintasvrp <amintas.pereira@ccc.ufcg.edu.br>
2019-05-20 23:08:32 -0300

    Adicionando tempo de expiração do token e melhor tratamento dele

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,11 +2,23 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 module.exports = (req, res, next) => {
-  const token = req.header('Authorization');
-  if (!token) {
+  const authHeader = req.header('Authorization');
+  if (!authHeader) {
     const err = new Error('Access denied. No token provided.');
     err.status = 401;
     return next(err);
+  }
+
+  const parts = authHeader.split(' ');
+
+  if (!parts.length === 2) {
+    return res.status(401).send({ error: 'Token error' });
+  }
+
+  const [scheme, token] = parts;
+
+  if (!/^Bearer$/i.test(scheme)) {
+    return res.status(401).send({ error: 'Token Malformatted' });
   }
 
   try {
--- modules/tokenGenerator.js
+++ modules/tokenGenerator.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 function sendToken (req) {
-  const token = jwt.sign({ username: req.body.username }, config.get('jwtPrivateKey'), { expiresIn: '7 days' });
+  const token = jwt.sign({ username: req.body.username }, config.get('jwtPrivateKey'), { expiresIn: 604800 });
   return token;
 }
 
