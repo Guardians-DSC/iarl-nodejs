db5e672bcc700a77dd42864cba4264e44e7fac6b
Amintas Victor <amintas.pereira@ccc.ufcg.edu.br>
2019-05-22 19:25:14 -0300

    Alterando comentário
    Está havendo um problema de compilação pelo Travis CI. O iarl-nodejs já foi testado em ubuntu 18.04

--- modules/tokenGenerator.js
+++ modules/tokenGenerator.js
@@ -3,7 +3,7 @@ const config = require('config');
 
 function sendToken (req) {
   // Validade do Token: 7 dias 
-  const token = jwt.sign({ username: req.body.username }, config.get('jwtPrivateKey'), { expiresIn: 604800 }); // 604800 = seconds * minutes * hours * days = 60 * 60 * 24 * 7
+  const token = jwt.sign({ username: req.body.username }, config.get('jwtPrivateKey'), { expiresIn: 604800 }); // 7 dias em segundos
   return token;
 }
 
