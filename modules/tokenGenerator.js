508b892e5b053e9e1a98eed40a59287b1da6b98a
amintasvrp <amintas.pereira@ccc.ufcg.edu.br>
2019-05-22 18:54:49 -0300

    Refatorando para passar no Travis CI

--- modules/tokenGenerator.js
+++ modules/tokenGenerator.js
@@ -2,7 +2,8 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 function sendToken (req) {
-  const token = jwt.sign({ username: req.body.username }, config.get('jwtPrivateKey'), { expiresIn: 604800 }); // Validade: 7 dias 
+  // Validade do Token: 7 dias 
+  const token = jwt.sign({ username: req.body.username }, config.get('jwtPrivateKey'), { expiresIn: 604800 });
   return token;
 }
 
