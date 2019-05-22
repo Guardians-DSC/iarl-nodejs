3d3becc5e3a8e2322847c77b6018397e4feee90a
amintasvrp <amintas.pereira@ccc.ufcg.edu.br>
2019-05-22 18:28:33 -0300

    Documentando tempo de expiração do token

--- modules/tokenGenerator.js
+++ modules/tokenGenerator.js
@@ -2,7 +2,9 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 function sendToken (req) {
-  const token = jwt.sign({ username: req.body.username }, config.get('jwtPrivateKey'), { expiresIn: 604800 });
+  const token = jwt.sign({ username: req.body.username }, 
+    config.get('jwtPrivateKey'), 
+    { expiresIn: 604800 }); // Validade: 7 dias 
   return token;
 }
 
