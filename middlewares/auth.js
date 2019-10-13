ac024ed7c116693ed96f869ddf4c251c2b90078b
amintasvrp <amintas.pereira@ccc.ufcg.edu.br>
2019-10-13 11:13:07 -0300

    Add short living for token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,18 +9,6 @@ module.exports = (req, res, next) => {
     return next(err);
   }
 
-  const parts = authHeader.split(' ');
-
-  if (!parts.length === 2) {
-    return res.status(401).send({ error: 'Token error' });
-  }
-
-  const [scheme, token] = parts;
-
-  if (!/^Bearer$/i.test(scheme)) {
-    return res.status(401).send({ error: 'Token Malformatted' });
-  }
-
   try {
     const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
     req.user = decoded;
