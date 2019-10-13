b4eae2d0970d91ae8f8f609ae6c6e31a694d354c
Amintas Victor <amintas.pereira@ccc.ufcg.edu.br>
2019-10-13 17:20:26 -0300

    Update tokenGenerator.js

--- modules/tokenGenerator.js
+++ modules/tokenGenerator.js
@@ -3,7 +3,7 @@ const config = require('config');
 
 function sendToken (req) {
   // Token time limit: 16 hours 
-  const token = jwt.sign({ username: req.body.username }, config.get('jwtPrivateKey'), { expiresIn: "16h" });
+  const token = jwt.sign({ username: req.body.username }, config.get('jwtPrivateKey'), { expiresIn: '16h' });
   return token;
 }
 
